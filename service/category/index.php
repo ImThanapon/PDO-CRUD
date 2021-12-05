52d1072f9d7b22957b47293b799bd494dd055877
Thanapon Visedsang <Thanapon.vi@ku.th>
2021-12-05 12:53:50 +0700

    add header access control

--- service/category/index.php
+++ service/category/index.php
@@ -1,5 +1,10 @@
 <?php
-    header('Content-Type: application/json');
+    header("Access-Control-Allow-Origin: *");
+    header("Content-Type: application/json; charset=UTF-8");
+    header("Access-Control-Allow-Methods: OPTIONS,GET,POST,PUT,DELETE");
+    header("Access-Control-Max-Age: 3600");
+    header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
     require_once '../connect.php';
 
     if($_SERVER['REQUEST_METHOD'] === 'GET'){
